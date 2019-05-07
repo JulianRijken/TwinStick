7e1c73cfbc5b99cb958f9bce5b7978dbefe797c7
enzo marguerink <enzomarquerink@gmail.com>
2019-05-07 10:37:22 +0200

    Ai, Score, Ai patrol

---
+++ TwinStick_Unity/Assets/Recources/Scripts/AI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void Update()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(Player.transform.position);
+    }
+}
