51ae2d89d06eadb7bc0f78f5a8a9b536bc1246f2
Julian <Rijkenjulian@gmail.com>
2019-05-07 17:13:59 +0200

    Final_Fix

--- TwinStick_Unity/Assets/Recources/Scripts/Player.cs
+++ TwinStick_Unity/Assets/Recources/Scripts/Player.cs
@@ -5,48 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Player : Damageable
 {
+
     private Rigidbody rig = null;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    [SerializeField] private float moveSpeed;
-
-    [SerializeField] private float rotationSpeed;
-=======
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float rotationSpeed = 30f;
->>>>>>> master
-=======
-    [SerializeField] private float moveSpeed;
 
-
-
-    [SerializeField] private float rotationSpeed;
->>>>>>> parent of 5b81819... Stats + GameManager
-
-    private void Start()
+    void Start()
     {
         rig = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-
         rig.MovePosition(transform.position + GetInput().normalized * Time.deltaTime * moveSpeed);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        float yRot = Quaternion.LookRotation(GetMousePos() - transform.position, Vector3.up).eulerAngles.y;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yRot, 0), Time.deltaTime * rotationSpeed);
-=======
-=======
-
-
-
->>>>>>> parent of 5b81819... Stats + GameManager
         float yRot = Quaternion.LookRotation(GetMousePos() - transform.position,Vector3.up).eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.Euler(0, yRot,0),Time.deltaTime * rotationSpeed);
->>>>>>> master
     }
 
     /// <summary>
@@ -54,9 +29,9 @@ public class Player : Damageable
     /// </summary>
     private Vector3 GetInput()
     {
-        return new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        return new Vector3(Input.GetAxisRaw("Horizontal"), 0,Input.GetAxisRaw("Vertical")); 
     }
-
+            
     /// <summary>
     /// Returns The Mouse Pos
     /// </summary>
@@ -70,12 +45,5 @@ public class Player : Damageable
 
         return cameraRay.GetPoint(rayLength);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-=======
 
-=======
->>>>>>> parent of 5b81819... Stats + GameManager
 }
->>>>>>> master
