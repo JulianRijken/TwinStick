03ecf36e7458022218cbf4bd36d9fd5c0ce0ee4a
Julian <Rijkenjulian@gmail.com>
2019-05-09 23:36:51 +0200

    Test Arms + New Camera Movement "WORK IN PROGRESS"

---
+++ TwinStick_Unity/Assets/MousePointer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = GetMousePos(cam);
+    }
+
+    /// <summary>
+    /// Returns The Mouse Pos
+    /// </summary>
+    private Vector3 GetMousePos(Camera cam)
+    {
+        Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        float rayLength;
+        groundPlane.Raycast(cameraRay, out rayLength);
+
+        return cameraRay.GetPoint(rayLength);
+    }
+}
