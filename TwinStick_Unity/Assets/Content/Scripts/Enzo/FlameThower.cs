b59516d375f3ae051ba9a8be16f75ac4c59884f1
enzo marguerink <enzomarquerink@gmail.com>
2019-05-16 16:27:14 +0200

    flamethrower

---
+++ TwinStick_Unity/Assets/Content/Scripts/Enzo/FlameThower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThower : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem FlameThrower;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        FlameThrower.Stop();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FlameThrower.Play();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            FlameThrower.Stop();
+        }
+    }
+}
