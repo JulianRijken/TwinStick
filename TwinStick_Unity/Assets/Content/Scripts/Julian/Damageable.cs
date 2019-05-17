d5dd59d2163b549b5634f5ee1389bcf0df5f8ce2
Julian <Rijkenjulian@gmail.com>
2019-05-17 23:30:03 +0200

    Damageble update

--- TwinStick_Unity/Assets/Content/Scripts/Julian/Damageable.cs
+++ TwinStick_Unity/Assets/Content/Scripts/Julian/Damageable.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Damageable : MonoBehaviour
 {
+
     [Header("Health")]
-    [SerializeField] protected float health = 100f;
-    [SerializeField] protected float maxHealth = 100f;
-    [SerializeField] protected bool invincible = false;
+    public float health = 100f;
+    public float maxHealth = 100f;
+    public bool invincible = false;
 
     [Header("Armor")]
-    [SerializeField] protected float armor = 100f;
-    [SerializeField] protected float armorProtectionProcent = 50f;
-    [SerializeField] protected float armorDamageMultiplyer = 0.1f;
-    [SerializeField] protected float maxArmor = 100f;
-    [SerializeField] protected bool armorEnabled = true;
+    public bool armorEnabled = true;
+    public float armorHealth = 100f;
+    public float armorProtectionProcent = 50f;
+    public float armorStrength = 10f;
+    public float maxArmorHealth = 100f;
 
     protected bool died;
 
@@ -36,7 +38,7 @@ public class Damageable : MonoBehaviour
     /// </summary>
     public float GetArmor()
     {
-        return armor;
+        return armorHealth;
     }
 
     /// <summary>
@@ -52,21 +54,23 @@ public class Damageable : MonoBehaviour
     /// </summary>
     public float GetMaxArmor()
     {
-        return maxArmor;
+        return maxArmorHealth;
     }
+
+
 
     /// <summary>
     /// Removes Health
     /// </summary>
-    public void DoDamage(float damage,string removedBy)
+    public void DoDamage(float damage, string removedBy)
     {
         if (!invincible)
         {
-            if(armorEnabled)
+            if (armorEnabled)
             {
-                if(armor > 0)
+                if (armorHealth > 0)
                 {
-                    armor -= damage * armorDamageMultiplyer;
+                    armorHealth -= damage / armorStrength;
                     float _damageProtected = (damage / 100) * armorProtectionProcent;
                     damage -= _damageProtected;
                 }
@@ -86,15 +90,18 @@ public class Damageable : MonoBehaviour
         }
     }
 
-
     /// <summary>
-    /// Removes the health over time
+    /// Does the damage over the time
     /// </summary>
     public void DoDamage(float damage, float overTime, string removedBy)
     {
         // Zorgt dat je een noramale void kan roepen inplaats van start Courutine
         StartCoroutine(IDoDamage(damage, overTime, removedBy));
     }
+
+    /// <summary>
+    ///  Does the damage over the time
+    /// </summary>
     private IEnumerator IDoDamage(float damage, float overTime, string removedBy)
     {
         float timer = 0;
@@ -112,6 +119,7 @@ public class Damageable : MonoBehaviour
     }
 
 
+
     /// <summary>
     /// Returns The current health
     /// </summary>
@@ -124,18 +132,21 @@ public class Damageable : MonoBehaviour
     /// <summary>
     /// Returns The current armor
     /// </summary>
-    public void AddArmor(float _armor)
+    public void AddArmorHealth(float _armorHealth)
     {
-        armor += _armor;
-        armor = Mathf.Clamp(armor, 0, maxArmor);
+        armorHealth += _armorHealth;
+        armorHealth = Mathf.Clamp(armorHealth, 0, maxArmorHealth);
     }
 
 
+    /// <summary>
+    /// Is Called When health Is 0 or lower with a name
+    /// </summary>
+    protected virtual void OnDeath(string diedBy) { }
 
     /// <summary>
     /// Is Called When health Is 0 or lower
     /// </summary>
-    protected virtual void OnDeath(string diedBy) { }
     protected virtual void OnDeath() { }
 
     /// <summary>
