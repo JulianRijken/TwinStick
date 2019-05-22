ffb11411193d76691b1b176f3c36e850d55d1c86
enzo marguerink <enzomarquerink@gmail.com>
2019-05-22 12:00:00 +0200

    added a timer

---
+++ TwinStick_Unity/Assets/Content/Scripts/Enzo/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    //private float timeleft = 30.0f;
+    private float currCountdownValue;
+
+    public IEnumerator StartCountdown(float countdownValue = 10)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartCountdown());
+    }
+
+    private void Update()
+    {
+        //timeleft -= Time.deltaTime;
+        //if (timeleft < 0)
+        //{
+        //    SceneManager.LoadScene(0);
+        //}
+        if (currCountdownValue < 1)
+        {
+            //SceneManager.LoadScene(0);
+            Debug.Log("Gameover");
+        }
+    }
+}
