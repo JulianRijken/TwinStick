2fdd16f36b6bf718f96f1a911bca9ef797b77374
enzo marguerink <enzomarquerink@gmail.com>
2019-06-03 09:55:22 +0200

    train mechanic

---
+++ TwinStick_Unity/Assets/Content/Scripts/Enzo/Enemybetweenwalls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemybetweenwalls : MonoBehaviour
+{
+    public Transform Startpoint;
+    public Transform Endpoint;
+
+    public float speed = 1.0f;
+
+    private float StartTime;
+
+    private float JourneyLength;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartTime = Time.time;
+
+        JourneyLength = Vector3.Distance(Startpoint.position, Endpoint.position);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float distCoverd = (Time.time - StartTime) * speed;
+
+        float fracJourney = distCoverd / JourneyLength;
+
+        transform.position = Vector3.Lerp(Startpoint.position, Endpoint.position, fracJourney);
+    }
+}
