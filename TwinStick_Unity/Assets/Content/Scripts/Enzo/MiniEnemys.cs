dc7c10233564ef070a981984b915adbea814af39
enzo marguerink <enzomarquerink@gmail.com>
2019-06-03 11:46:37 +0200

    small enemys

---
+++ TwinStick_Unity/Assets/Content/Scripts/Enzo/MiniEnemys.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MiniEnemys : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(Player.transform.position);
+    }
+}
