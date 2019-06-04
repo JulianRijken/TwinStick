5fd3cc4ea86043a89cb4e619469a1a5ed1844e0d
Julian <Rijkenjulian@gmail.com>
2019-06-05 00:25:32 +0200

    Tutorial Level Revamped! + Hinge doors

---
+++ TwinStick_Unity/Assets/LightBlink.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBlink : MonoBehaviour
+{
+    [SerializeField] private Light lamp;
+    [SerializeField] private float lerpSpeed;
+    [SerializeField] private Vector2 randomTimeBitween;
+    [SerializeField] private Vector2 randomIntensityBitween;
+
+
+    float toIntensity;
+
+    IEnumerator Start()
+    {
+        if(lamp == null)
+        {
+            lamp = GetComponent<Light>();
+        }
+
+        if (lamp == null)
+            Destroy(this);
+
+
+        while (true)
+        {
+            // random intensity
+            toIntensity = Random.Range(randomIntensityBitween.x, randomIntensityBitween.y);
+
+            // random time
+            yield return new WaitForSeconds(Random.Range(randomTimeBitween.x, randomTimeBitween.y));
+        }
+    }
+
+    private void Update()
+    {
+            lamp.intensity = Mathf.Lerp(lamp.intensity, toIntensity,Time.deltaTime * lerpSpeed);
+
+    }
+}
