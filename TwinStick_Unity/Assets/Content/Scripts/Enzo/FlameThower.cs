c8b2f84463f0bfbb41fbed06c814f8c086e5c563
Julian <Rijkenjulian@gmail.com>
2019-06-06 10:38:42 +0200

    FlameThrower FIxed

--- TwinStick_Unity/Assets/Content/Scripts/Enzo/FlameThower.cs
+++ TwinStick_Unity/Assets/Content/Scripts/Enzo/FlameThower.cs
@@ -7,10 +7,15 @@ public class FlameThower : Weapon
     [SerializeField] private ParticleSystem[] particleSystems = null;
     [SerializeField] private AudioSource sound = null;
 
+    public override void OnActive()
+    {
+        base.OnActive();
+        sound.Pause();
+    }
 
     protected override void OnAttack()
     {
-
+        
         sound.UnPause();
 
         for (int i = 0; i < particleSystems.Length; i++)
