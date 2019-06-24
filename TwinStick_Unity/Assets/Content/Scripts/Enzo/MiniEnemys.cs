c068c14b48f2dec38c91d190e178315e2d3e9c83
enzo marguerink <enzomarquerink@gmail.com>
2019-06-24 11:34:49 +0200

    minienemy

--- TwinStick_Unity/Assets/Content/Scripts/Enzo/MiniEnemys.cs
+++ TwinStick_Unity/Assets/Content/Scripts/Enzo/MiniEnemys.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class MiniEnemys : Damageable
 {
     public GameObject Player;
+    [SerializeField] private LayerMask hitLayer;
     //public float dealdamage;
 
     // Update is called once per frame
@@ -24,11 +25,14 @@ public class MiniEnemys : Damageable
         //Music.Stop();
     }
 
-    //private void OnTriggerEnter(Collider collider_enemy)
-    //{
-    //    if (collider_enemy.gameObject.tag == "player")
-    //    {
-    //        maxHealth -= 5;
-    //    }
-    //}
+    private void OnCollisionEnter(Collision collider_enemy)
+    {
+        Damageable _damageble = collider_enemy.gameObject.GetComponent<Damageable>();
+        Debug.Log((1 << collider_enemy.gameObject.layer) + " " + hitLayer.value);
+        if (_damageble != null && 1 << collider_enemy.gameObject.layer == hitLayer.value)
+        {
+            Debug.Log("DAMAGE");
+            _damageble.DoDamage(5, "Rat");
+        }
+    }
 }
