37946d76ab088c4888a5c7514dfc3de6a7cacddf
Julian <Rijkenjulian@gmail.com>
2019-06-24 10:35:37 +0200

    Knife Damage

---
+++ TwinStick_Unity/Assets/AnimatorEventHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AnimatorEventHelper : MonoBehaviour
+{
+    public delegate void AnimationEventTrigger();
+
+    public event AnimationEventTrigger OnAnimationEventTrigger;
+
+    public void HandleAnimationEvent()
+    {
+        OnAnimationEventTrigger?.Invoke();
+    }
+
+}
--- TwinStick_Unity/Assets/Content/Scripts/Julian/Knife.cs
+++ TwinStick_Unity/Assets/Content/Scripts/Julian/Knife.cs
@@ -1,13 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class Knife : Weapon
 {
+    [SerializeField] private AnimatorEventHelper animatorEventHelper;
+    [SerializeField] private float damage;
+    [SerializeField] private LayerMask hitLayer;
+    [SerializeField] private GameObject targetHitSound = null;
+
     public override void OnActive()
     {
         base.OnActive();
         GameManager.instance.notificationCenter.FireGunMagAmmoChange(0, 0);
+        animatorEventHelper.OnAnimationEventTrigger += HandleOnAnimationEventTrigger;
+    }
+
+    private void HandleOnAnimationEventTrigger()
+    {
+        Collider[] _hitColliders = Physics.OverlapSphere(transform.position,1f, hitLayer);
+
+        for (int i = 0; i < _hitColliders.Length; i++)
+        {       
+            Damageable _damageble = _hitColliders[i].GetComponent<Damageable>();
+            if (_damageble != null)
+            {
+                _damageble.DoDamage(damage, "Knife");
+
+                if (targetHitSound != null)
+                    Instantiate(targetHitSound);
+            }
+        }
+
+    }
+
+    public override void OnInActive()
+    {
+        base.OnInActive();
+        animatorEventHelper.OnAnimationEventTrigger -= HandleOnAnimationEventTrigger;
     }
 
 
