9d80509fa17be4f4fc79e14c51d38005bda7b8ad
EnzoMarguerink <45165520+EnzoMarguerink@users.noreply.github.com>
2020-10-26 20:23:38 +0100

    Update Tongue.cs

--- TwinStick_Unity/Assets/Content/Scripts/Enzo/Tongue.cs
+++ TwinStick_Unity/Assets/Content/Scripts/Enzo/Tongue.cs
@@ -40,39 +40,24 @@ public class Tongue : Damageable
 
             if (dist <= aggressiveRange && dist > attackRange)
             {
-                //print("Hee, je komt de dicht bij....ratel...ratel!");
                 state = TongueState.aggressive;
             }
             else if (dist <= attackRange)
             {
-                //print("ik ben heel boos en ga nu aanvallen!");
                 state = TongueState.attacking;
             }
             else if (dist > aggressiveRange)
             {
-                //print("ik ben chill!");
                 state = TongueState.passive;
-                //GetComponent<NavMeshAgent>().SetDestination(Player.transform.position);
             }
         }
 
         UpdateAnimatorValues();
     }
 
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    player = other.GetComponent<Player>();
+    
 
-    //    if (player != null)
-    //    {
-    //        state = TongueState.aggressive;
-    //    }
-    //}
-
-    //public void OnTriggerExit(Collider other)
-    //{
-    //    state = TongueState.passive;
-    //}
+   
 
     public void UpdateAnimatorValues()
     {
